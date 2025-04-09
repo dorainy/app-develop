402ae88cab2795b936c15e7138ca06109500f5bf
dorainy <dorainy.bayona30@gmail.com>
2025-04-08 20:38:18 -0500

    Inicio de sesión con js

---
+++ js/loginService.js
@@ -0,0 +1,24 @@
+document.getElementById('loginForm').addEventListener('submit', function(e){
+	e.preventDefault();
+	const email = document.getElementById('email').value;
+	const password = document.getElementById('password').value;
+	let mensaje = ''
+	let tipoAlerta = ''
+	if (email === '' || password === ''){
+		mensaje = 'Por favor comleta todos los campos.'
+		tipoAlerta = 'warning'
+	}
+	else if(email === 'prueba@gmail.com' && password === '123456'){
+		mensaje = 'Inicio de sesion exitoso.'
+		tipoAlerta = 'success'
+	}
+	else{
+		mensaje = 'correo o contraseña incorrectos.'
+		tipoAlerta = 'danger'
+	}
+	let alerta = `<div class="alert alert-${tipoAlerta} alert-dismissible fade show" role="alert">
+		${mensaje}
+		<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+		</div>`;
+	document.getElementById('mensaje').innerHTML = alerta;
+})
